2d18772baaad0f95a113261c5dc2ae9defe8f806
inferno214221 <inferno214221@gmail.com>
2026-03-29 22:57:22 +1030

    Add rustdocs generation action for GitHub.

--- flake.nix
+++ flake.nix
@@ -46,6 +46,72 @@
         devShells.default = mkShell {
           inherit nativeBuildInputs;
         };
+
+        packages.docs-raw = (naersk.buildPackage rec {
+          src = ./.;
+
+          inherit nativeBuildInputs;
+
+          mode = "check";
+          doDoc = true;
+          doDocFail = true;
+          cargoDocCommands = let
+            advancedOptions = ''
+              -- \
+              --theme ${src}/doc/kali-dark.css \
+              --html-in-header ${./doc/robots.html} \
+              --enable-index-page \
+              --generate-macro-expansion \
+              -Z unstable-options
+            # '';
+            # ^ This is really bad but we just use a bash open comment here so that eval doesn't
+            # complain about "unexpected syntax token near '||'". We need doDocFail anyway, so the
+            # generated code is `|| false`. (No effect)
+          in old: [
+            "cargo $cargo_options rustdoc -p standard-lib ${advancedOptions}"
+            "cargo $cargo_options rustdoc -p ct-regex --all-features ${advancedOptions}"
+            "cargo $cargo_options rustdoc -p ct-regex-internal --all-features ${advancedOptions}"
+          ];
+
+          postInstall = ''
+            cp $src/doc/robots.txt $out/
+            cp $src/doc/CNAME $out/
+          '';
+        }).doc;
+
+        packages.docs = let
+          findAndSed = "find ./standard_lib ./ct_regex ./ct_regex_internal -type f -name \"*html\" -exec sed -E";
+        in stdenv.mkDerivation {
+          name = "compile-time-regex-doc";
+          version = "0.1.0";
+          src = "${packages.docs-raw}";
+
+          buildPhase = ''
+            # Highlight keywords
+            ${findAndSed} "s/(>|>([^\">]*[; \[\(])?)(((pub|const|fn|self|Self|struct|enum|type|impl|for|unsafe|as|mut) ?)+)([<& \n:,\)])/\1<span class=\"extra-kw\">\3<\/span>\6/g" -i {} \;
+            # Second pass for references and pointers
+            ${findAndSed} "s/(>|>([^\">]*[; \[\(]*)?)(mut|const) /\1<span class=\"extra-kw\">\3<\/span> /g" -i {} \;
+            # Highlight operators
+            ${findAndSed} "s/(>|>([^\">]*[; \[\(\w])?)(&amp;|-&gt;|::|\*)([^/])/\1<span class=\"extra-op\">\3<\/span>\4/g" -i {} \;
+            # Where
+            ${findAndSed} "s/(<div class=\"where\">)(where)/\1<span class=\"extra-kw\">\2<\/span>/g" -i {} \;
+            # TODO: '\w+, mut, <>, (), []
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -R ./* $out/
+          '';
+        };
+
+        apps.docs = {
+          type = "app";
+          program = "${(
+            writeShellScript
+              "open-docs"
+              "${xdg-utils}/bin/xdg-open ${packages.docs}/index.html"
+          )}";
+        };
       }
     );
 }

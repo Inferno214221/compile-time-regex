6f5f527412f6bb613c8c465d91b6f5e253f54283
inferno214221 <inferno214221@gmail.com>
2026-05-05 17:21:47 +0930

    Separates Matcher trait from primitive implementations.

--- internal/src/matcher/mod.rs
+++ internal/src/matcher/mod.rs
@@ -1,12 +1,14 @@
 pub mod capture;
 pub mod composite;
 pub mod helper;
+pub mod interface;
 pub mod primitive;
 pub mod quantifier;
 
 pub use capture::*;
 pub use composite::*;
 pub use helper::*;
+pub use interface::*;
 pub use primitive::*;
 pub use quantifier::*;
 
--- internal/src/matcher/primitive.rs
+++ internal/src/matcher/primitive.rs
@@ -1,61 +1,6 @@
 use std::fmt::{self, Debug};
 
-use crate::{expr::IndexedCaptures, haystack::{HaystackItem, HaystackOf}, matcher::{impl_all_captures_single, impl_all_matches_single}};
-
-// New strategy: use Iterators. Lazy can wrap quantifiers which implement a Quantifier trait or
-// similar. Lazy's iterator method calls all_matches_lazy or similar and does nothing else.
-
-pub trait Matcher<I: HaystackItem>: Debug + Default {
-    /// Checks if the start of the haystack contains a match for this [`Matcher`]. If this method
-    /// successfully matches the start of the haystack, `hay` is progressed so that `hay.item()`
-    /// hasn't been matched yet. On a fail, the state of hay is undefined.
-    fn matches<'a, H: HaystackOf<'a, I>>(hay: &mut H) -> bool;
-
-    type AllMatches<'a, H: HaystackOf<'a, I>>: Iterator<Item = usize>;
-
-    // It would be nice to use a custom Iterator here rather than a Vec, but reversing an arbitrary
-    // match is not easy, so we just progress through linearly and store them all.
-    // This could cause issues with huge haystacks, but: all regexes need to be compiled at compile
-    // time and are hence controlled by the author. If their pattern will be operating on huge
-    // haystacks and need backtracking, that's up to them.
-
-    /// Produces a Vec of all valid haystack states produced as the result of a valid match at the
-    /// start of `hay`, used to implement backtracking. The Vec is produced in reverse priority
-    /// order, so the last match has the highest priority. After calling all_matches, the state of
-    /// `hay` itself is undefined.
-    ///
-    /// # Required
-    /// This method needs to be implemented by all [`Matcher`]s that can match more than one string
-    /// of characters from a haystack.
-    fn all_matches<'a, H: HaystackOf<'a, I>>(hay: &mut H) -> Self::AllMatches<'a, H>;
-
-    /// Checks if the start of the haystack contains a match for this Matcher, writing any groups
-    /// to `caps`. Similar to [`matches`], this method progresses `hay` and `caps` on a success. On
-    /// a fail, they have undefined states.
-    ///
-    /// # Required
-    /// This method needs to be implemented for capturing groups or any type that holds other
-    /// [`Matcher`]s, so that it can redirect to the relevant `capture` methods.
-    fn captures<'a, H: HaystackOf<'a, I>>(hay: &mut H, caps: &mut IndexedCaptures) -> bool {
-        let _ = caps;
-        Self::matches(hay)
-    }
-
-    type AllCaptures<'a, H: HaystackOf<'a, I>>: Iterator<Item = (usize, IndexedCaptures)>;
-
-    /// Produces a Vec of all valid captures (and accompanying haystack states) present at the start
-    /// of `hay`. Used to implement backtracking for capturing methods. As with
-    /// [`all_matches`](Matcher::all_matches), the resulting Vec is produced in reverse priority
-    /// order. After calling all_captures, the state of `hay` and `caps` are undefined.
-    ///
-    /// # Required
-    /// This method needs to be implemented for any type that also implements
-    /// [`captures`](Matcher::captures) and [`all_matches`](Matcher::all_matches).
-    fn all_captures<'a, H: HaystackOf<'a, I>>(
-        hay: &mut H,
-        caps: &mut IndexedCaptures
-    ) -> Self::AllCaptures<'a, H>;
-}
+use crate::{expr::IndexedCaptures, haystack::{HaystackItem, HaystackOf}, matcher::{Matcher, impl_all_captures_single, impl_all_matches_single}};
 
 #[derive(Default)]
 pub struct Byte<const N: u8>;

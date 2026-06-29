8e04a3ccfdc4c701f13622bf1a405212a24d9c74
Inferno214221 <inferno214221@gmail.com>
2026-06-29 13:15:49 +0930

    Adds binary benchmark script.

---
+++ bench-bins.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+cargo build --release --bins
+
+hyperfine -N -w 5 './target/release/ct-regex-capture email \
+    "me@example.com" "spam@example.com" "example@gmail.com" "dotless@email" "@example.com" "example.com"' -n "My Compile Time Regex" \
+    './target/release/regex-capture email "me@example.com" "spam@example.com" "example@gmail.com" "dotless@email" "@example.com" "example.com"' -n "Regex Crate"
+    
+hyperfine -N -w 5 './target/release/ct-regex-capture phonenum \
+    "+12123456789" "0123456789" "+9876543210" "0123321789" "567890" "+234E2"' -n "My Compile Time Regex" \
+    './target/release/regex-capture phonenum "+12123456789" "0123456789" "+9876543210" "0123321789" "567890" "+234E2"' -n "Regex Crate"

48aeb2c823325b48c1ebbce2fe6087fc5ecc55eb
Inferno214221 <inferno214221@gmail.com>
2026-07-29 22:42:27 +0930

    Fixes directory assumptions for benchmarking script.

--- bench-bins.sh
+++ bench-bins.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
+cd bench
 cargo build --release --bins
+cd ..
 
 hyperfine -N -w 5 './target/release/ct-regex-capture email \
     "me@example.com" "spam@example.com" "example@gmail.com" "dotless@email" "@example.com" "example.com"' -n "My Compile Time Regex" \

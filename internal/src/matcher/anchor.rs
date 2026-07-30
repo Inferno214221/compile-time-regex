24431bd6e7c4be4e47c4aea9e6de3cec5bae05b3
Inferno214221 <inferno214221@gmail.com>
2026-07-30 13:18:57 +0930

    Replace Anchor type with set of traits.

--- internal/src/matcher/anchor.rs
+++ internal/src/matcher/anchor.rs
@@ -1,31 +1,9 @@
+use std::{fmt::Debug, marker::PhantomData};
 use std::ops::ControlFlow;
 
-use regex_syntax::hir::Properties;
+use crate::{haystack::{Haystack, HaystackSlice}, sealed::Sealed};
 
-use crate::haystack::{Haystack, HaystackSlice};
-
-#[derive(Debug, Clone, Copy)]
-pub struct Anchors {
-    pub min_len: Option<usize>,
-    pub max_len: Option<usize>,
-    pub start: bool,
-    pub end: bool,
-}
-
-pub enum AnchorHint {
-    Continue,
-}
-
-impl Anchors {
-    pub fn from_properties(props: &Properties) -> Anchors {
-        Anchors {
-            min_len: props.minimum_len(),
-            max_len: props.maximum_len(),
-            start: props.look_set_prefix().contains_anchor_haystack(),
-            end: props.look_set_suffix().contains_anchor_haystack(),
-        }
-    }
-
+pub trait Anchor: Sealed + Debug + Default + Clone + Copy {
     /// Asserts that each anchor in this set could possibly succeed with the given haystack state.
     /// In the presence of a start anchor, the haystack's position at the start doesn't need to be
     /// checked again.
@@ -41,35 +19,129 @@ impl Anchors {
     /// - If the value of the outer type is [`ControlFlow::Continue`], the inner [`bool`] represents
     ///   whether the current position should be checked for a match. A value of `false` indicates
     ///   that the haystack should be progressed before checking again.
-    pub fn assert<'a, H: Haystack<'a>>(
-        &self,
-        hay: &H
-    ) -> ControlFlow<(), bool> {
-        if self.start && !hay.is_start() {
-            return ControlFlow::Break(());
-        }
-        let len = hay.remainder_as_slice().as_bytes().len();
-        if let Some(min) = self.min_len && len < min {
-            return ControlFlow::Break(());
-        }
-        if self.end && let Some(max) = self.max_len && len > max {
-            return ControlFlow::Continue(false);
-        }
-        ControlFlow::Continue(true)
-    }
+    fn assert<'a, H: Haystack<'a>>(hay: &H) -> ControlFlow<(), bool>;
 
     /// Asserts that each anchor in this set could possibly succeed with the given haystack state.
     /// In the presence of a start anchor, the haystack's position at the start doesn't need to be
     /// checked again. This variant provide no information about whether the search should continue
     /// and should be called by searches that intend to match the entirety of the provided haystack.
-    pub fn assert_fixed<'a, H: Haystack<'a>>(&self, hay: &H) -> bool {
-        let len = hay.remainder_as_slice().as_bytes().len();
-        if let Some(min) = self.min_len && len < min {
-            return false;
+    fn assert_fixed<'a, H: Haystack<'a>>(hay: &H) -> bool;
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Start;
+
+impl Sealed for Start {}
+
+impl Anchor for Start {
+    fn assert<'a, H: Haystack<'a>>(hay: &H) -> ControlFlow<(), bool> {
+        if hay.is_start() {
+            ControlFlow::Continue(true)
+        } else {
+            ControlFlow::Break(())
         }
-        if self.end && let Some(max) = self.max_len && len > max {
-            return false;
-        }
+    }
+
+    fn assert_fixed<'a, H: Haystack<'a>>(_hay: &H) -> bool {
         true
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct MinLen<const N: usize>;
+
+impl<const N: usize> Sealed for MinLen<N> {}
+
+impl<const N: usize> Anchor for MinLen<N> {
+    fn assert<'a, H: Haystack<'a>>(hay: &H) -> ControlFlow<(), bool> {
+        if hay.remainder_as_slice().as_bytes().len() >= N {
+            ControlFlow::Continue(true)
+        } else {
+            ControlFlow::Break(())
+        }
+    }
+
+    fn assert_fixed<'a, H: Haystack<'a>>(hay: &H) -> bool {
+        hay.remainder_as_slice().as_bytes().len() >= N
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct MaxLen<const N: usize>;
+
+impl<const N: usize> Sealed for MaxLen<N> {}
+
+impl<const N: usize> Anchor for MaxLen<N> {
+    fn assert<'a, H: Haystack<'a>>(_hay: &H) -> ControlFlow<(), bool> {
+        ControlFlow::Continue(true)
+    }
+
+    fn assert_fixed<'a, H: Haystack<'a>>(hay: &H) -> bool {
+        hay.remainder_as_slice().as_bytes().len() <= N
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct EndAndMaxLen<const N: usize>;
+
+impl<const N: usize> Sealed for EndAndMaxLen<N> {}
+
+impl<const N: usize> Anchor for EndAndMaxLen<N> {
+    fn assert<'a, H: Haystack<'a>>(hay: &H) -> ControlFlow<(), bool> {
+        if hay.remainder_as_slice().as_bytes().len() <= N {
+            ControlFlow::Continue(true)
+        } else {
+            ControlFlow::Continue(false)
+        }
+    }
+
+    fn assert_fixed<'a, H: Haystack<'a>>(hay: &H) -> bool {
+        hay.remainder_as_slice().as_bytes().len() <= N
+    }
+}
+
+impl Sealed for () {}
+
+impl Anchor for () {
+    fn assert<'a, H: Haystack<'a>>(_hay: &H) -> ControlFlow<(), bool> {
+        ControlFlow::Continue(true)
+    }
+
+    fn assert_fixed<'a, H: Haystack<'a>>(_hay: &H) -> bool {
+        true
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct AnchorPair<A: Anchor, B: Anchor>(pub PhantomData<(A, B)>);
+
+impl<A: Anchor, B: Anchor> Sealed for AnchorPair<A, B> {}
+
+impl<A: Anchor, B: Anchor> Anchor for AnchorPair<A, B> {
+    fn assert<'a, H: Haystack<'a>>(hay: &H) -> ControlFlow<(), bool> {
+        ControlFlow::Continue(
+            A::assert(hay)? && B::assert(hay)?
+        )
+    }
+
+    fn assert_fixed<'a, H: Haystack<'a>>(hay: &H) -> bool {
+        A::assert_fixed(hay) && B::assert_fixed(hay)
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct AnchorSet<A: Anchor, B: Anchor, C: Anchor>(pub PhantomData<(A, B, C)>);
+
+impl<A: Anchor, B: Anchor, C: Anchor> Sealed for AnchorSet<A, B, C> {}
+
+impl<A: Anchor, B: Anchor, C: Anchor> Anchor for AnchorSet<A, B, C> {
+    fn assert<'a, H: Haystack<'a>>(hay: &H) -> ControlFlow<(), bool> {
+        ControlFlow::Continue(
+            A::assert(hay)? && B::assert(hay)? && C::assert(hay)?
+        )
+    }
+
+    fn assert_fixed<'a, H: Haystack<'a>>(hay: &H) -> bool {
+        A::assert_fixed(hay) && B::assert_fixed(hay) && C::assert_fixed(hay)
     }
 }

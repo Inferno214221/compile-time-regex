08649b58173708fa055ba2551af398ee0b5dbb6c
Inferno214221 <inferno214221@gmail.com>
2026-08-02 16:32:19 +0930

    Adds zero-width tests for the remaining Regex functions.

--- src/tests/zero_width.rs
+++ src/tests/zero_width.rs
@@ -1,30 +1,42 @@
 use std::iter;
 use std::ops::Range;
 
+use ct_regex_internal::expr::CaptureFromRanges;
+
 use super::*;
 
 regex! {
     pub StartExpr = r"^"
 }
 
-fn start_cap_to_parts<'a>(cap: StartExprCapture<'a, &'a str>) -> (Range<usize>, &'a str) {
-    (cap.whole_match_range(), cap.whole_match())
-}
-
 regex! {
     pub EndExpr = r"$"
-}
-
-fn end_cap_to_parts<'a>(cap: EndExprCapture<'a, &'a str>) -> (Range<usize>, &'a str) {
-    (cap.whole_match_range(), cap.whole_match())
 }
 
 regex! {
     pub EmptyExpr = r""
 }
 
-fn empty_cap_to_parts<'a>(cap: EmptyExprCapture<'a, &'a str>) -> (Range<usize>, &'a str) {
+fn cap_to_parts<'a, R, const N: usize>(cap: R::Capture<'a, &'a str>) -> (Range<usize>, &'a str)
+where
+    R: Regex<char, N>,
+{
     (cap.whole_match_range(), cap.whole_match())
+}
+
+fn quote_capture<'a, R, const N: usize>(cap: R::Capture<'a, &'a str>) -> String
+where
+    R: Regex<char, N>,
+{
+    format!("'{}'", cap.whole_match())
+}
+
+fn create_counter() -> impl FnMut() -> String {
+    let mut count = 0;
+    move || {
+        count += 1;
+        count.to_string()
+    }
 }
 
 #[test]
@@ -202,59 +214,56 @@ fn slice_all_matches_overlapping() {
 
 #[test]
 fn do_capture() {
-    fn do_capture<'a, R, F, const N: usize>(hay: &'a str, f: F) -> Option<(Range<usize>, &'a str)>
+    fn do_capture<R, const N: usize>(hay: &str) -> Option<(Range<usize>, &str)>
     where
         R: Regex<char, N>,
-        F: FnOnce(<R as Regex<char, N>>::Capture<'a, &'a str>) -> (Range<usize>, &'a str),
     {
-        R::do_capture(hay).map(f)
+        R::do_capture(hay).map(cap_to_parts::<R, _>)
     }
 
-    assert_eq!(do_capture::<StartExpr, _, _>("", start_cap_to_parts), Some((0..0, "")));
-    assert_eq!(do_capture::<StartExpr, _, _>("a", start_cap_to_parts), None);
-    assert_eq!(do_capture::<StartExpr, _, _>("abc def", start_cap_to_parts), None);
+    assert_eq!(do_capture::<StartExpr, _>(""), Some((0..0, "")));
+    assert_eq!(do_capture::<StartExpr, _>("a"), None);
+    assert_eq!(do_capture::<StartExpr, _>("abc def"), None);
 
-    assert_eq!(do_capture::<EndExpr, _, _>("", end_cap_to_parts), Some((0..0, "")));
-    assert_eq!(do_capture::<EndExpr, _, _>("a", end_cap_to_parts), None);
-    assert_eq!(do_capture::<EndExpr, _, _>("abc def", end_cap_to_parts), None);
+    assert_eq!(do_capture::<EndExpr, _>(""), Some((0..0, "")));
+    assert_eq!(do_capture::<EndExpr, _>("a"), None);
+    assert_eq!(do_capture::<EndExpr, _>("abc def"), None);
 
-    assert_eq!(do_capture::<EmptyExpr, _, _>("", empty_cap_to_parts), Some((0..0, "")));
-    assert_eq!(do_capture::<EmptyExpr, _, _>("a", empty_cap_to_parts), None);
-    assert_eq!(do_capture::<EmptyExpr, _, _>("abc def", empty_cap_to_parts), None);
+    assert_eq!(do_capture::<EmptyExpr, _>(""), Some((0..0, "")));
+    assert_eq!(do_capture::<EmptyExpr, _>("a"), None);
+    assert_eq!(do_capture::<EmptyExpr, _>("abc def"), None);
 }
 
 #[test]
 fn find_capture() {
-    fn find_capture<'a, R, F, const N: usize>(hay: &'a str, f: F) -> Option<(Range<usize>, &'a str)>
+    fn find_capture<R, const N: usize>(hay: &str) -> Option<(Range<usize>, &str)>
     where
         R: Regex<char, N>,
-        F: FnOnce(<R as Regex<char, N>>::Capture<'a, &'a str>) -> (Range<usize>, &'a str),
     {
-        R::find_capture(hay).map(f)
+        R::find_capture(hay).map(cap_to_parts::<R, _>)
     }
 
-    assert_eq!(find_capture::<StartExpr, _, _>("", start_cap_to_parts), Some((0..0, "")));
-    assert_eq!(find_capture::<StartExpr, _, _>("a", start_cap_to_parts), Some((0..0, "")));
-    assert_eq!(find_capture::<StartExpr, _, _>("abc def", start_cap_to_parts), Some((0..0, "")));
+    assert_eq!(find_capture::<StartExpr, _>(""), Some((0..0, "")));
+    assert_eq!(find_capture::<StartExpr, _>("a"), Some((0..0, "")));
+    assert_eq!(find_capture::<StartExpr, _>("abc def"), Some((0..0, "")));
 
-    assert_eq!(find_capture::<EndExpr, _, _>("", end_cap_to_parts), Some((0..0, "")));
-    assert_eq!(find_capture::<EndExpr, _, _>("a", end_cap_to_parts), Some((1..1, "")));
-    assert_eq!(find_capture::<EndExpr, _, _>("abc def", end_cap_to_parts), Some((7..7, "")));
+    assert_eq!(find_capture::<EndExpr, _>(""), Some((0..0, "")));
+    assert_eq!(find_capture::<EndExpr, _>("a"), Some((1..1, "")));
+    assert_eq!(find_capture::<EndExpr, _>("abc def"), Some((7..7, "")));
 
-    assert_eq!(find_capture::<EmptyExpr, _, _>("", empty_cap_to_parts), Some((0..0, "")));
-    assert_eq!(find_capture::<EmptyExpr, _, _>("a", empty_cap_to_parts), Some((0..0, "")));
-    assert_eq!(find_capture::<EmptyExpr, _, _>("abc def", empty_cap_to_parts), Some((0..0, "")));
+    assert_eq!(find_capture::<EmptyExpr, _>(""), Some((0..0, "")));
+    assert_eq!(find_capture::<EmptyExpr, _>("a"), Some((0..0, "")));
+    assert_eq!(find_capture::<EmptyExpr, _>("abc def"), Some((0..0, "")));
 }
 
 #[allow(clippy::single_range_in_vec_init)]
 #[test]
 fn find_all_captures_non_overlapping() {
-    fn collect_all_captures<'a, R, F, const N: usize>(hay: &'a str, f: F) -> Vec<(Range<usize>, &'a str)>
+    fn collect_all_captures<R, const N: usize>(hay: &str) -> Vec<(Range<usize>, &str)>
     where
         R: Regex<char, N>,
-        F: FnMut(<R as Regex<char, N>>::Capture<'a, &'a str>) -> (Range<usize>, &'a str),
     {
-        R::find_all_captures(hay, false).map(f).collect::<Vec<_>>()
+        R::find_all_captures(hay, false).map(cap_to_parts::<R, _>).collect::<Vec<_>>()
     }
 
     fn zip_literal(vec: Vec<Range<usize>>) -> Vec<(Range<usize>, &'static str)> {
@@ -263,18 +272,18 @@ fn find_all_captures_non_overlapping() {
         ).collect::<Vec<_>>()
     }
 
-    assert_eq!(collect_all_captures::<StartExpr, _, _>("", start_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<StartExpr, _, _>("a", start_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<StartExpr, _, _>("abc def", start_cap_to_parts), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<StartExpr, _>(""), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<StartExpr, _>("a"), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<StartExpr, _>("abc def"), zip_literal(vec![0..0]));
 
-    assert_eq!(collect_all_captures::<EndExpr, _, _>("", end_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<EndExpr, _, _>("a", end_cap_to_parts), zip_literal(vec![1..1]));
-    assert_eq!(collect_all_captures::<EndExpr, _, _>("abc def", end_cap_to_parts), zip_literal(vec![7..7]));
+    assert_eq!(collect_all_captures::<EndExpr, _>(""), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<EndExpr, _>("a"), zip_literal(vec![1..1]));
+    assert_eq!(collect_all_captures::<EndExpr, _>("abc def"), zip_literal(vec![7..7]));
 
-    assert_eq!(collect_all_captures::<EmptyExpr, _, _>("", empty_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<EmptyExpr, _, _>("a", empty_cap_to_parts), zip_literal(vec![0..0, 1..1]));
+    assert_eq!(collect_all_captures::<EmptyExpr, _>(""), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<EmptyExpr, _>("a"), zip_literal(vec![0..0, 1..1]));
     assert_eq!(
-        collect_all_captures::<EmptyExpr, _, _>("abc def", empty_cap_to_parts),
+        collect_all_captures::<EmptyExpr, _>("abc def"),
         zip_literal(vec![0..0, 1..1, 2..2, 3..3, 4..4, 5..5, 6..6, 7..7])
     );
 }
@@ -282,12 +291,11 @@ fn find_all_captures_non_overlapping() {
 #[allow(clippy::single_range_in_vec_init)]
 #[test]
 fn find_all_captures_overlapping() {
-    fn collect_all_captures<'a, R, F, const N: usize>(hay: &'a str, f: F) -> Vec<(Range<usize>, &'a str)>
+    fn collect_all_captures<R, const N: usize>(hay: &str) -> Vec<(Range<usize>, &str)>
     where
         R: Regex<char, N>,
-        F: FnMut(<R as Regex<char, N>>::Capture<'a, &'a str>) -> (Range<usize>, &'a str),
     {
-        R::find_all_captures(hay, true).map(f).collect::<Vec<_>>()
+        R::find_all_captures(hay, true).map(cap_to_parts::<R, _>).collect::<Vec<_>>()
     }
 
     fn zip_literal(vec: Vec<Range<usize>>) -> Vec<(Range<usize>, &'static str)> {
@@ -296,20 +304,145 @@ fn find_all_captures_overlapping() {
         ).collect::<Vec<_>>()
     }
 
-    assert_eq!(collect_all_captures::<StartExpr, _, _>("", start_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<StartExpr, _, _>("a", start_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<StartExpr, _, _>("abc def", start_cap_to_parts), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<StartExpr, _>(""), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<StartExpr, _>("a"), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<StartExpr, _>("abc def"), zip_literal(vec![0..0]));
 
-    assert_eq!(collect_all_captures::<EndExpr, _, _>("", end_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<EndExpr, _, _>("a", end_cap_to_parts), zip_literal(vec![1..1]));
-    assert_eq!(collect_all_captures::<EndExpr, _, _>("abc def", end_cap_to_parts), zip_literal(vec![7..7]));
+    assert_eq!(collect_all_captures::<EndExpr, _>(""), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<EndExpr, _>("a"), zip_literal(vec![1..1]));
+    assert_eq!(collect_all_captures::<EndExpr, _>("abc def"), zip_literal(vec![7..7]));
 
-    assert_eq!(collect_all_captures::<EmptyExpr, _, _>("", empty_cap_to_parts), zip_literal(vec![0..0]));
-    assert_eq!(collect_all_captures::<EmptyExpr, _, _>("a", empty_cap_to_parts), zip_literal(vec![0..0, 1..1]));
+    assert_eq!(collect_all_captures::<EmptyExpr, _>(""), zip_literal(vec![0..0]));
+    assert_eq!(collect_all_captures::<EmptyExpr, _>("a"), zip_literal(vec![0..0, 1..1]));
     assert_eq!(
-        collect_all_captures::<EmptyExpr, _, _>("abc def", empty_cap_to_parts),
+        collect_all_captures::<EmptyExpr, _>("abc def"),
         zip_literal(vec![0..0, 1..1, 2..2, 3..3, 4..4, 5..5, 6..6, 7..7])
     );
 }
 
-// TODO: Replace tests.
+#[test]
+fn replace() {
+    fn replace<R: Regex<char, N>, const N: usize>(hay: &str, success: bool) -> String {
+        let mut hay = String::from(hay);
+        assert_eq!(R::replace(&mut hay, "r"), success);
+        hay
+    }
+
+    assert_eq!(replace::<StartExpr, _>("", true), "r");
+    assert_eq!(replace::<StartExpr, _>("a", true), "ra");
+    assert_eq!(replace::<StartExpr, _>("abc def", true), "rabc def");
+
+    assert_eq!(replace::<EndExpr, _>("", true), "r");
+    assert_eq!(replace::<EndExpr, _>("a", true), "ar");
+    assert_eq!(replace::<EndExpr, _>("abc def", true), "abc defr");
+
+    assert_eq!(replace::<EmptyExpr, _>("", true), "r");
+    assert_eq!(replace::<EmptyExpr, _>("a", true), "ra");
+    assert_eq!(replace::<EmptyExpr, _>("abc def", true), "rabc def");
+}
+
+#[test]
+fn replace_all() {
+    fn replace_all<R: Regex<char, N>, const N: usize>(hay: &str, count: usize) -> String {
+        let mut hay = String::from(hay);
+        assert_eq!(R::replace_all(&mut hay, "r"), count);
+        hay
+    }
+
+    assert_eq!(replace_all::<StartExpr, _>("", 1), "r");
+    assert_eq!(replace_all::<StartExpr, _>("a", 1), "ra");
+    assert_eq!(replace_all::<StartExpr, _>("abc def", 1), "rabc def");
+
+    assert_eq!(replace_all::<EndExpr, _>("", 1), "r");
+    assert_eq!(replace_all::<EndExpr, _>("a", 1), "ar");
+    assert_eq!(replace_all::<EndExpr, _>("abc def", 1), "abc defr");
+
+    assert_eq!(replace_all::<EmptyExpr, _>("", 1), "r");
+    assert_eq!(replace_all::<EmptyExpr, _>("a", 2), "rar");
+    assert_eq!(replace_all::<EmptyExpr, _>("abc def", 8), "rarbrcr rdrerfr");
+}
+
+#[test]
+fn replace_all_using() {
+    fn replace_all_using<R: Regex<char, N>, const N: usize>(hay: &str, count: usize) -> String {
+        let mut hay = String::from(hay);
+        assert_eq!(R::replace_all_using(&mut hay, create_counter()), count);
+        hay
+    }
+
+    assert_eq!(replace_all_using::<StartExpr, _>("", 1), "1");
+    assert_eq!(replace_all_using::<StartExpr, _>("a", 1), "1a");
+    assert_eq!(replace_all_using::<StartExpr, _>("abc def", 1), "1abc def");
+
+    assert_eq!(replace_all_using::<EndExpr, _>("", 1), "1");
+    assert_eq!(replace_all_using::<EndExpr, _>("a", 1), "a1");
+    assert_eq!(replace_all_using::<EndExpr, _>("abc def", 1), "abc def1");
+
+    assert_eq!(replace_all_using::<EmptyExpr, _>("", 1), "1");
+    assert_eq!(replace_all_using::<EmptyExpr, _>("a", 2), "1a2");
+    assert_eq!(replace_all_using::<EmptyExpr, _>("abc def", 8), "1a2b3c4 5d6e7f8");
+}
+
+#[test]
+fn replace_using_iter() {
+    fn replace_using_iter<R: Regex<char, N>, const N: usize>(hay: &str, count: usize) -> String {
+        let iter = (1..=2).map(|n| n.to_string());
+        let mut hay = String::from(hay);
+        assert_eq!(R::replace_using_iter(&mut hay, iter), count);
+        hay
+    }
+
+    assert_eq!(replace_using_iter::<StartExpr, _>("", 1), "1");
+    assert_eq!(replace_using_iter::<StartExpr, _>("a", 1), "1a");
+    assert_eq!(replace_using_iter::<StartExpr, _>("abc def", 1), "1abc def");
+
+    assert_eq!(replace_using_iter::<EndExpr, _>("", 1), "1");
+    assert_eq!(replace_using_iter::<EndExpr, _>("a", 1), "a1");
+    assert_eq!(replace_using_iter::<EndExpr, _>("abc def", 1), "abc def1");
+
+    assert_eq!(replace_using_iter::<EmptyExpr, _>("", 1), "1");
+    assert_eq!(replace_using_iter::<EmptyExpr, _>("a", 2), "1a2");
+    assert_eq!(replace_using_iter::<EmptyExpr, _>("abc def", 2), "1a2bc def");
+}
+
+#[test]
+fn replace_captured() {
+    fn replace_captured<R: Regex<char, N>, const N: usize>(hay: &str, success: bool) -> String {
+        let mut hay = String::from(hay);
+        assert_eq!(R::replace_captured(&mut hay, quote_capture::<R, _>), success);
+        hay
+    }
+
+    assert_eq!(replace_captured::<StartExpr, _>("", true), "''");
+    assert_eq!(replace_captured::<StartExpr, _>("a", true), "''a");
+    assert_eq!(replace_captured::<StartExpr, _>("abc def", true), "''abc def");
+
+    assert_eq!(replace_captured::<EndExpr, _>("", true), "''");
+    assert_eq!(replace_captured::<EndExpr, _>("a", true), "a''");
+    assert_eq!(replace_captured::<EndExpr, _>("abc def", true), "abc def''");
+
+    assert_eq!(replace_captured::<EmptyExpr, _>("", true), "''");
+    assert_eq!(replace_captured::<EmptyExpr, _>("a", true), "''a");
+    assert_eq!(replace_captured::<EmptyExpr, _>("abc def", true), "''abc def");
+}
+
+#[test]
+fn replace_all_captured() {
+    fn replace_all_captured<R: Regex<char, N>, const N: usize>(hay: &str, count: usize) -> String {
+        let mut hay = String::from(hay);
+        assert_eq!(R::replace_all_captured(&mut hay, quote_capture::<R, _>), count);
+        hay
+    }
+
+    assert_eq!(replace_all_captured::<StartExpr, _>("", 1), "''");
+    assert_eq!(replace_all_captured::<StartExpr, _>("a", 1), "''a");
+    assert_eq!(replace_all_captured::<StartExpr, _>("abc def", 1), "''abc def");
+
+    assert_eq!(replace_all_captured::<EndExpr, _>("", 1), "''");
+    assert_eq!(replace_all_captured::<EndExpr, _>("a", 1), "a''");
+    assert_eq!(replace_all_captured::<EndExpr, _>("abc def", 1), "abc def''");
+
+    assert_eq!(replace_all_captured::<EmptyExpr, _>("", 1), "''");
+    assert_eq!(replace_all_captured::<EmptyExpr, _>("a", 2), "''a''");
+    assert_eq!(replace_all_captured::<EmptyExpr, _>("abc def", 8), "''a''b''c'' ''d''e''f''");
+}
